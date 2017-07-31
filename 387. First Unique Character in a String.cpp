029ea60cdce6f3a2bcc6baa16deba9b510775ad5
HlG4399 <CaiQinyi@foxmail.com>
2017-07-31 16:52:51 +0800

    Add files via upload

---
+++ 387. First Unique Character in a String.cpp
@@ -0,0 +1,32 @@
+/*
+Problem Description:
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+*/
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> ch_nums(26,0);
+        for(auto ch:s)
+        {
+            ch_nums[ch-97]++;
+        }       
+        for(int i=0;i!=s.size();i++)
+        {
+            if(ch_nums[s[i]-97]==1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
