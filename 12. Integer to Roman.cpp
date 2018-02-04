44724537f8bcb7c345cd22763814772cb376bcde
Qinyi Cai <CaiQinyi@foxmail.com>
2018-02-04 10:07:19 +0800

    Add files via upload

---
+++ 12. Integer to Roman.cpp
@@ -0,0 +1,19 @@
+/*
+Problem Description:
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string map[4][10]={
+                {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+                {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+                {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+                {"", "M", "MM", "MMM"}
+        };
+        return map[3][num/1000]+map[2][num/100%10]+map[1][num/10%10]+map[0][num%10];
+    }
+};
