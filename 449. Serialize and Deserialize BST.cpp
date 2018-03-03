d29d56e13215bbaa73415948635c1c5ad81f6e8c
Qinyi Cai <CaiQinyi@foxmail.com>
2018-03-03 14:24:06 +0800

    Add files via upload

---
+++ 449. Serialize and Deserialize BST.cpp
@@ -0,0 +1,63 @@
+/*
+Problem Description:
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+The encoded string should be as compact as possible.
+
+Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        ostringstream os;
+        serialize(root,os);
+        return os.str();
+    }
+    
+    void serialize(TreeNode* root,ostringstream& os)
+    {
+        if(!root)
+        {
+            os<<"# ";
+            return;
+        }
+        os<<to_string(root->val)<<" ";
+        serialize(root->left,os);
+        serialize(root->right,os);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream is(data);      
+        return deserialize(is);
+    }
+    
+    TreeNode* deserialize(istringstream& is)
+    {
+        string s="";
+        is>>s;
+        if(s=="#") return NULL;
+        TreeNode* root=new TreeNode(stoi(s));
+        root->left=deserialize(is);
+        root->right=deserialize(is);
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
