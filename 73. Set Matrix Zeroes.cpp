b207a84ed5440b059ef9a325c83bd89b36f4e2ad
Qinyi Cai <CaiQinyi@foxmail.com>
2018-05-24 16:18:18 +0800

    Add files via upload

---
+++ 73. Set Matrix Zeroes.cpp
@@ -0,0 +1,71 @@
+/*
+Problem Description:
+Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+
+Example 1:
+
+Input: 
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+Output: 
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+Example 2:
+
+Input: 
+[
+  [0,1,2,0],
+  [3,4,5,2],
+  [1,3,1,5]
+]
+Output: 
+[
+  [0,0,0,0],
+  [0,4,5,0],
+  [0,3,1,0]
+]
+Follow up:
+
+A straight forward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+*/
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        if(m==0) return;
+        int n=matrix[0].size();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    for(int k=0;k<n;k++)
+                    {
+                        if(k!=j && matrix[i][k]!=0) matrix[i][k]=INT_MAX-1;
+                    }
+                    for(int k=0;k<m;k++)
+                    {
+                        if(k!=i && matrix[k][j]!=0) matrix[k][j]=INT_MAX-1;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==INT_MAX-1) matrix[i][j]=0;
+            }
+        }
+    }
+};
