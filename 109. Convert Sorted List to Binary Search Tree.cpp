74120c9cb1e6d0e687d36091cfd6e34e90ca9e4b
Qinyi Cai <CaiQinyi@foxmail.com>
2018-06-13 22:34:23 +0800

    Add files via upload

---
+++ 109. Convert Sorted List to Binary Search Tree.cpp
@@ -0,0 +1,59 @@
+/*
+Problem Description:
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted linked list: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(!head) return NULL;
+        vector<int> nums;
+        while(head)
+        {
+            nums.emplace_back(head->val);
+            head=head->next;
+        }
+        return DFS(nums,0,nums.size()-1);;
+    }
+    
+    TreeNode* DFS(vector<int>& nums,int l,int r)
+    {
+        if(l>r) return NULL;
+        int mid=(l+r)/2;
+        TreeNode* root=new TreeNode(nums[mid]);
+        root->left=DFS(nums,l,mid-1);
+        root->right=DFS(nums,mid+1,r);
+        return root;
+    }
+};
