9ac5929c2a42993dd1d87ca104e7d113cfe1afc7
Qinyi Cai <CaiQinyi@foxmail.com>
2018-10-24 10:18:57 +0800

    Add files via upload

---
+++ 922. Sort Array By Parity II.cpp
@@ -0,0 +1,45 @@
+/*
+Problem Description:
+Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
+
+Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
+
+You may return any answer array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+ 
+
+Note:
+
+2 <= A.length <= 20000
+A.length % 2 == 0
+0 <= A[i] <= 1000
+*/
+
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        int even=0,odd=1;
+        vector<int> results(A.size(),0);
+        for(auto a:A)
+        {
+            if(a%2==0)
+            {
+                results[even]=a;
+                even+=2;
+            }
+            else
+            {
+                results[odd]=a;
+                odd+=2;
+            }
+        }
+        return results;
+    }
+};
