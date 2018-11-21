775331ee49fb8b5b2debd287ea62f27161521b46
Qinyi Cai <CaiQinyi@foxmail.com>
2018-11-21 10:34:03 +0800

    Add files via upload

---
+++ 937. Reorder Log Files.cpp
@@ -0,0 +1,45 @@
+/*
+Problem Description:
+You have an array of logs.  Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+
+Each word after the identifier will consist only of lowercase letters, or;
+Each word after the identifier will consist only of digits.
+We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+ 
+
+Example 1:
+
+Input: ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+ 
+
+Note:
+
+0 <= logs.length <= 100
+3 <= logs[i].length <= 100
+logs[i] is guaranteed to have an identifier, and a word after the identifier.
+*/
+
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        stable_sort(logs.begin(),logs.end(),cmp);
+        return logs;
+    }
+    
+    static bool cmp(const string& log1,const string& log2)
+    {
+        string s1=log1.substr(log1.find(" ")+1);
+        string s2=log2.substr(log2.find(" ")+1);
+        if(isdigit(s1[0])) return false;
+        else if(isdigit(s2[0])) return true;
+        return s1.compare(s2)<0;
+    }
+};
