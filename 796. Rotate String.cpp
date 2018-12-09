449ca45773d35d456ea4a4fd598f65ef86df9f53
Qinyi Cai <CaiQinyi@foxmail.com>
2018-12-09 10:37:41 +0800

    Add files via upload

---
+++ 796. Rotate String.cpp
@@ -0,0 +1,26 @@
+/*
+Problem Description:
+We are given two strings, A and B.
+
+A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+
+Example 1:
+Input: A = 'abcde', B = 'cdeab'
+Output: true
+
+Example 2:
+Input: A = 'abcde', B = 'abced'
+Output: false
+Note:
+
+A and B will have length at most 100.
+*/
+
+class Solution {
+public:
+    bool rotateString(string A, string B) {
+        if(A.size()!=B.size()) return false;
+        string str=A+A;
+        return str.find(B)!=string::npos;
+    }
+};
