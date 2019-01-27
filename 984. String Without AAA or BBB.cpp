cbbe90ecfa8b27791705241e9139b89e0ecd272b
Qinyi Cai <CaiQinyi@foxmail.com>
2019-01-27 16:36:10 +0800

    Add files via upload

---
+++ 984. String Without AAA or BBB.cpp
@@ -0,0 +1,40 @@
+/*
+Problem Description:
+Given two integers A and B, return any string S such that:
+
+S has length A + B and contains exactly A 'a' letters, and exactly B 'b' letters;
+The substring 'aaa' does not occur in S;
+The substring 'bbb' does not occur in S.
+ 
+
+Example 1:
+
+Input: A = 1, B = 2
+Output: "abb"
+Explanation: "abb", "bab" and "bba" are all correct answers.
+Example 2:
+
+Input: A = 4, B = 1
+Output: "aabaa"
+ 
+
+Note:
+
+0 <= A <= 100
+0 <= B <= 100
+It is guaranteed such an S exists for the given A and B.
+*/
+
+class Solution {
+public:
+    string strWithout3a3b(int A, int B,char a='a',char b='b') {
+        string result="";
+        if(A<B) return strWithout3a3b(B, A,'b','a');
+        while(A>0 || B>0){
+            if(A>0) result+=a,--A;
+            if(A>B) result+=a,--A;
+            if(B>0) result+=b,--B;
+        }
+        return result;
+    }
+};
