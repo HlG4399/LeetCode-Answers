8d23ed109e716510d01df9c6a9750db97b5c826d
Qinyi Cai <CaiQinyi@foxmail.com>
2019-02-07 15:29:33 +0800

    Add files via upload

---
+++ 791. Custom Sort String.cpp
@@ -0,0 +1,37 @@
+/*
+Problem Description:
+S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+
+S was sorted in some custom order previously. We want to permute the characters of T so that they match the order that S was sorted. More specifically, if x occurs before y in S, then x should occur before y in the returned string.
+
+Return any permutation of T (as a string) that satisfies this property.
+
+Example :
+Input: 
+S = "cba"
+T = "abcd"
+Output: "cbad"
+Explanation: 
+"a", "b", "c" appear in S, so the order of "a", "b", "c" should be "c", "b", and "a". 
+Since "d" does not appear in S, it can be at any position in T. "dcba", "cdba", "cbda" are also valid outputs.
+ 
+
+Note:
+
+S has length at most 26, and no character is repeated in S.
+T has length at most 200.
+S and T consist of lowercase letters only.
+*/
+
+class Solution {
+public:
+    string customSortString(string S, string T) {
+        vector<int> pos_T(26,0),pos_S(26,0);
+        for(auto t:T) ++pos_T[t-'a'];
+        for(auto s:S) ++pos_S[s-'a'];
+        string result="";
+        for(auto s:S) result.append(pos_T[s-'a'],s);
+        for(auto t:T) if(pos_S[t-'a']==0) result+=t;
+        return result;
+    }
+};
