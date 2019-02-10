d892b23ffdddd3914ef50dbdaa8e4bfce255acad
Qinyi Cai <CaiQinyi@foxmail.com>
2019-02-10 17:56:32 +0800

    Add files via upload

---
+++ 986. Interval List Intersections.cpp
@@ -0,0 +1,45 @@
+/*
+Problem Description:
+Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+(Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
+
+Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
+ 
+
+Note:
+
+0 <= A.length < 1000
+0 <= B.length < 1000
+0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+*/
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> intervalIntersection(vector<Interval>& A, vector<Interval>& B) {
+        vector<Interval> results;
+        int n_A=A.size(),n_B=B.size();
+        int p=0,q=0;
+        while(p<n_A && q<n_B){
+            int start=max(A[p].start,B[q].start);
+            int end=min(A[p].end,B[q].end);
+            if(start<=end) results.emplace_back(Interval(start,end));
+            if(A[p].end<B[q].end) ++p;
+            else ++q;
+        }
+        return results;
+    }
+};
