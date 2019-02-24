51eeb575029a9571e8260dc208eff43266878498
Qinyi Cai <CaiQinyi@foxmail.com>
2019-02-24 13:16:03 +0800

    Add files via upload

---
+++ 932. Beautiful Array.cpp
@@ -0,0 +1,45 @@
+/*
+Problem Description:
+For some fixed N, an array A is beautiful if it is a permutation of the integers 1, 2, ..., N, such that:
+
+For every i < j, there is no k with i < k < j such that A[k] * 2 = A[i] + A[j].
+
+Given N, return any beautiful array A.  (It is guaranteed that one exists.)
+
+ 
+
+Example 1:
+
+Input: 4
+Output: [2,1,4,3]
+Example 2:
+
+Input: 5
+Output: [3,1,2,5,4]
+ 
+
+Note:
+
+1 <= N <= 1000
+*/
+
+class Solution {
+public:
+    vector<int> beautifulArray(int N) {
+        vector<int> result={1};
+        while(result.size()<N){
+            vector<int> tempVec;
+            int temp=0;
+            for(auto res:result){
+                temp=2*res-1;
+                if(temp<=N) tempVec.emplace_back(temp);
+            }
+            for(auto res:result){
+                temp=2*res;
+                if(temp<=N) tempVec.emplace_back(temp);
+            }
+            result=tempVec;
+        }
+        return result;
+    }
+};
