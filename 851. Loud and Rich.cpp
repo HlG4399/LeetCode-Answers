c4bed0663ca4e333ba222f9e1f22490cccc6aff1
Qinyi Cai <CaiQinyi@foxmail.com>
2019-03-12 11:14:42 +0800

    Add files via upload

---
+++ 851. Loud and Rich.cpp
@@ -0,0 +1,61 @@
+/*
+Problem Description:
+In a group of N people (labelled 0, 1, 2, ..., N-1), each person has different amounts of money, and different levels of quietness.
+
+For convenience, we'll call the person with label x, simply "person x".
+
+We'll say that richer[i] = [x, y] if person x definitely has more money than person y.  Note that richer may only be a subset of valid observations.
+
+Also, we'll say quiet[x] = q if person x has quietness q.
+
+Now, return answer, where answer[x] = y if y is the least quiet person (that is, the person y with the smallest value of quiet[y]), among all people who definitely have equal to or more money than person x.
+
+ 
+
+Example 1:
+
+Input: richer = [[1,0],[2,1],[3,1],[3,7],[4,3],[5,3],[6,3]], quiet = [3,2,5,4,6,1,7,0]
+Output: [5,5,2,5,4,5,6,7]
+Explanation: 
+answer[0] = 5.
+Person 5 has more money than 3, which has more money than 1, which has more money than 0.
+The only person who is quieter (has lower quiet[x]) is person 7, but
+it isn't clear if they have more money than person 0.
+
+answer[7] = 7.
+Among all people that definitely have equal to or more money than person 7
+(which could be persons 3, 4, 5, 6, or 7), the person who is the quietest (has lower quiet[x])
+is person 7.
+
+The other answers can be filled out with similar reasoning.
+Note:
+
+1 <= quiet.length = N <= 500
+0 <= quiet[i] < N, all quiet[i] are different.
+0 <= richer.length <= N * (N-1) / 2
+0 <= richer[i][j] < N
+richer[i][0] != richer[i][1]
+richer[i]'s are all different.
+The observations in richer are all logically consistent.
+*/
+
+class Solution {
+public:
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+        unordered_map<int,vector<int>> hash;
+        for(auto r:richer) hash[r[1]].emplace_back(r[0]);
+        int n=quiet.size();
+        vector<int> results(n,-1);
+        for(int i=0;i<n;++i) DFS(i,hash,quiet,results);
+        return results;
+    }
+    
+    int DFS(int i,unordered_map<int,vector<int>>& hash,vector<int>& quiet,vector<int>& results){
+        if(results[i]>0) return results[i];
+        results[i]=i;
+        for(auto j:hash[i]){
+            if(quiet[results[i]]>quiet[DFS(j,hash,quiet,results)]) results[i]=results[j];
+        }
+        return results[i];
+    }
+};
