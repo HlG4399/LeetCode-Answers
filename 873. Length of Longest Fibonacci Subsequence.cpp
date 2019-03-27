7a48b123aee0a7a5548a863b252178e98293eac5
Qinyi Cai <CaiQinyi@foxmail.com>
2019-03-27 10:15:13 +0800

    Add files via upload

---
+++ 873. Length of Longest Fibonacci Subsequence.cpp
@@ -0,0 +1,52 @@
+/*
+Problem Description:
+A sequence X_1, X_2, ..., X_n is fibonacci-like if:
+
+n >= 3
+X_i + X_{i+1} = X_{i+2} for all i + 2 <= n
+Given a strictly increasing array A of positive integers forming a sequence, find the length of the longest fibonacci-like subsequence of A.  If one does not exist, return 0.
+
+(Recall that a subsequence is derived from another sequence A by deleting any number of elements (including none) from A, without changing the order of the remaining elements.  For example, [3, 5, 8] is a subsequence of [3, 4, 5, 6, 7, 8].)
+
+ 
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7,8]
+Output: 5
+Explanation:
+The longest subsequence that is fibonacci-like: [1,2,3,5,8].
+Example 2:
+
+Input: [1,3,7,11,12,14,18]
+Output: 3
+Explanation:
+The longest subsequence that is fibonacci-like:
+[1,11,12], [3,11,14] or [7,11,18].
+ 
+
+Note:
+
+3 <= A.length <= 1000
+1 <= A[0] < A[1] < ... < A[A.length - 1] <= 10^9
+(The time limit has been reduced by 50% for submissions in Java, C, and C++.)
+*/
+
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& A) {
+        unordered_set<int> hash(A.begin(),A.end());
+        int n=A.size();
+        int a=0,b=0,temp_length=2;
+        int result=2;
+        for(int i=0;i<n;++i){
+            for(int j=i+1;j<n;++j){
+                a=A[i],b=A[j],temp_length=2;
+                while(hash.find(a+b)!=hash.end()) 
+                    b=a+b,a=b-a,++temp_length;
+                result=max(result,temp_length);
+            }
+        }
+        return result>2?result:0;
+    }
+};
