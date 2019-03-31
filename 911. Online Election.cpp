2c726065f872e7e6b888e66b0981f4a04a0f94d6
Qinyi Cai <CaiQinyi@foxmail.com>
2019-03-31 15:00:08 +0800

    Add files via upload

---
+++ 911. Online Election.cpp
@@ -0,0 +1,56 @@
+/*
+Problem Description:
+In an election, the i-th vote was cast for persons[i] at time times[i].
+
+Now, we would like to implement the following query function: TopVotedCandidate.q(int t) will return the number of the person that was leading the election at time t.  
+
+Votes cast at time t will count towards our query.  In the case of a tie, the most recent vote (among tied candidates) wins.
+
+ 
+
+Example 1:
+
+Input: ["TopVotedCandidate","q","q","q","q","q","q"], [[[0,1,1,0,0,1,0],[0,5,10,15,20,25,30]],[3],[12],[25],[15],[24],[8]]
+Output: [null,0,1,1,0,0,1]
+Explanation: 
+At time 3, the votes are [0], and 0 is leading.
+At time 12, the votes are [0,1,1], and 1 is leading.
+At time 25, the votes are [0,1,1,0,0,1], and 1 is leading (as ties go to the most recent vote.)
+This continues for 3 more queries at time 15, 24, and 8.
+ 
+
+Note:
+
+1 <= persons.length = times.length <= 5000
+0 <= persons[i] <= persons.length
+times is a strictly increasing array with all elements in [0, 10^9].
+TopVotedCandidate.q is called at most 10000 times per test case.
+TopVotedCandidate.q(int t) is always called with t >= times[0].
+*/
+
+class TopVotedCandidate {
+private:
+    map<int,int> querys;
+public:
+    TopVotedCandidate(vector<int> persons, vector<int> times) {
+        int n=persons.size();
+        vector<int> votes(n,0);
+        for(int i=0,vote_leading=-1;i<n;++i){
+            ++votes[persons[i]];
+            if(vote_leading<=votes[persons[i]]){
+                vote_leading=votes[persons[i]];
+                querys[times[i]]=persons[i];
+            }
+        }
+    }
+    
+    int q(int t) {
+        return (--querys.upper_bound(t))->second;
+    }
+};
+
+/**
+ * Your TopVotedCandidate object will be instantiated and called as such:
+ * TopVotedCandidate* obj = new TopVotedCandidate(persons, times);
+ * int param_1 = obj->q(t);
+ */
