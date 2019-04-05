0d9699e6aa4e65d3cf96ad266520a518234dcb47
Qinyi Cai <CaiQinyi@foxmail.com>
2019-04-05 10:10:28 +0800

    Add files via upload

---
+++ 470. Implement Rand10() Using Rand7().cpp
@@ -0,0 +1,44 @@
+/*
+Problem Description:
+Given a function rand7 which generates a uniform random integer in the range 1 to 7, write a function rand10 which generates a uniform random integer in the range 1 to 10.
+
+Do NOT use system's Math.random().
+
+ 
+
+Example 1:
+
+Input: 1
+Output: [7]
+Example 2:
+
+Input: 2
+Output: [8,4]
+Example 3:
+
+Input: 3
+Output: [8,1,10]
+ 
+
+Note:
+
+rand7 is predefined.
+Each testcase has one argument: n, the number of times that rand10 is called.
+ 
+
+Follow up:
+
+What is the expected value for the number of calls to rand7() function?
+Could you minimize the number of calls to rand7()?
+*/
+
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        return (rand7()+rand7()+rand7()+rand7()+rand7())%10+1;
+    }
+};
