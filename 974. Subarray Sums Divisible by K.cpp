1986c708c7218366022ee536c34a6819e4b4c7a9
Qinyi Cai <CaiQinyi@foxmail.com>
2019-04-10 10:14:48 +0800

    Add files via upload

---
+++ 974. Subarray Sums Divisible by K.cpp
@@ -0,0 +1,34 @@
+/*
+Problem Description:
+Given an array A of integers, return the number of (contiguous, non-empty) subarrays that have a sum divisible by K.
+
+ 
+
+Example 1:
+
+Input: A = [4,5,0,-2,-3,1], K = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by K = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+ 
+
+Note:
+
+1 <= A.length <= 30000
+-10000 <= A[i] <= 10000
+2 <= K <= 10000
+*/
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& A, int K) {
+        int n=A.size();
+        vector<int> prefixSum(n+1,0);
+        for(int i=1;i<n+1;++i) prefixSum[i]=prefixSum[i-1]+A[i-1];
+        unordered_map<int,int> hash;
+        for(int i=0;i<n+1;++i) ++hash[(prefixSum[i]%K+K)%K];
+        int result=0;
+        for(auto it=hash.begin();it!=hash.end();++it) result+=(it->second)*(it->second-1)/2;
+        return result;
+    }
+};
